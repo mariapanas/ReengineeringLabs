270d6616e1313c25297af8412a2ce248382a74f9
mariapanas <8061828@stud.kai.edu.ua>
2025-11-24 23:59:16 +0200

    Додано пряму залежність між NetSdrClientApp і EchoTspServer. У UI створено клас ClassToTestReffrences для демонстрації цієї залежності. Додано тестовий клас ArchitectureTests із правилом перевірки, яке навмисно спричиняє падіння тесту за наявності прямої залежності.

---
+++ NetSdrClientApp/ClassToTestReffrences.cs
@@ -0,0 +1,12 @@
+﻿using EchoTspServer;
+
+namespace NetSdrClientApp
+{
+    public class ClassToTestReffrences
+    {
+        public void CreateServer()
+        {
+            var server = new EchoServer(5000);
+        }
+    }
+}
